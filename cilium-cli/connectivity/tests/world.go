c67790b8acdaef0d348543461a54c8ef5e9b53ef
Tobias Klauser <tobias@cilium.io>
2024-09-18 15:34:43 +0200

    cilium-cli/connectivity: allow to set retry options on PodToWorld2 scenario
    This allows to set retry options for the scenario testing HTTPS
    connectivity to cilium.io. The same is already possible for the
    PodToWorld scenario.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- cilium-cli/connectivity/tests/world.go
+++ cilium-cli/connectivity/tests/world.go
@@ -70,14 +70,19 @@ func (s *podToWorld) Run(ctx context.Context, t *check.Test) {
 	}
 }
 
-// PodToWorld2 sends an HTTPS request to cilium.io from from random client
-// Pods.
-func PodToWorld2() check.Scenario {
-	return &podToWorld2{}
+// PodToWorld2 sends an HTTPS request to cilium.io from each client Pod.
+func PodToWorld2(opts ...RetryOption) check.Scenario {
+	cond := &retryCondition{}
+	for _, op := range opts {
+		op(cond)
+	}
+	return &podToWorld2{rc: cond}
 }
 
 // podToWorld2 implements a Scenario.
-type podToWorld2 struct{}
+type podToWorld2 struct {
+	rc *retryCondition
+}
 
 func (s *podToWorld2) Name() string {
 	return "pod-to-world-2"
@@ -96,8 +101,9 @@ func (s *podToWorld2) Run(ctx context.Context, t *check.Test) {
 
 	for _, client := range ct.ClientPods() {
 		// With https, over port 443.
+		httpsOpts := s.rc.CurlOptions(https, features.IPFamilyAny, client, ct.Params())
 		t.NewAction(s, fmt.Sprintf("https-cilium-io-%d", i), &client, https, features.IPFamilyAny).Run(func(a *check.Action) {
-			a.ExecInPod(ctx, ct.CurlCommand(https, features.IPFamilyAny))
+			a.ExecInPod(ctx, ct.CurlCommand(https, features.IPFamilyAny, httpsOpts...))
 			a.ValidateFlows(ctx, client, a.GetEgressRequirements(fp))
 			a.ValidateMetrics(ctx, client, a.GetEgressMetricsRequirements())
 		})
